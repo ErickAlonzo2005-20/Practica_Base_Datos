c9698af704d381d3ed649571e352c1672dd71912
ErickAlonzo2005-20 <gomezalonzo237@gmail.com>
2025-10-25 00:06:48 -0400

    Categoria

---
+++ Categoria.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace EjercicioBaseDeDatosWinForm
+{
+    partial class Categorias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dgClientes = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.txtNombreCategoria = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.textCategoriaID = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtIDActualizar = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNombreActualizado = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgClientes)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnCargar);
+            this.groupBox4.Location = new System.Drawing.Point(105, 270);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox4.Size = new System.Drawing.Size(1232, 80);
+            this.groupBox4.TabIndex = 26;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Mostrar";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(17, 21);
+            this.btnCargar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(1193, 50);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dgClientes
+            // 
+            this.dgClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgClientes.Location = new System.Drawing.Point(123, 354);
+            this.dgClientes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgClientes.Name = "dgClientes";
+            this.dgClientes.RowHeadersWidth = 51;
+            this.dgClientes.RowTemplate.Height = 24;
+            this.dgClientes.Size = new System.Drawing.Size(1215, 187);
+            this.dgClientes.TabIndex = 22;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ID);
+            this.groupBox1.Controls.Add(this.txtNombreCategoria);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.textCategoriaID);
+            this.groupBox1.Location = new System.Drawing.Point(100, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(425, 246);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar";
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(232, 63);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(118, 16);
+            this.ID.TabIndex = 2;
+            this.ID.Text = "Nombre Categoria";
+            // 
+            // txtNombreCategoria
+            // 
+            this.txtNombreCategoria.Location = new System.Drawing.Point(233, 81);
+            this.txtNombreCategoria.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNombreCategoria.Name = "txtNombreCategoria";
+            this.txtNombreCategoria.Size = new System.Drawing.Size(185, 22);
+            this.txtNombreCategoria.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID Categoria";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(113, 121);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(189, 41);
+            this.btnAgregar.TabIndex = 12;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click_1);
+            // 
+            // textCategoriaID
+            // 
+            this.textCategoriaID.Location = new System.Drawing.Point(23, 81);
+            this.textCategoriaID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textCategoriaID.Name = "textCategoriaID";
+            this.textCategoriaID.Size = new System.Drawing.Size(185, 22);
+            this.textCategoriaID.TabIndex = 5;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(6, 115);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(301, 41);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click_1);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(229, 115);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(189, 41);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtIDActualizar);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.txtNombreActualizado);
+            this.groupBox3.Controls.Add(this.btnActualizar);
+            this.groupBox3.Location = new System.Drawing.Point(897, 18);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(440, 246);
+            this.groupBox3.TabIndex = 24;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Actualizar";
+            // 
+            // txtIDActualizar
+            // 
+            this.txtIDActualizar.Location = new System.Drawing.Point(6, 54);
+            this.txtIDActualizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtIDActualizar.Name = "txtIDActualizar";
+            this.txtIDActualizar.Size = new System.Drawing.Size(185, 22);
+            this.txtIDActualizar.TabIndex = 16;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(2, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(82, 16);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "ID Categoria";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Nombre Categoria";
+            // 
+            // txtNombreActualizado
+            // 
+            this.txtNombreActualizado.Location = new System.Drawing.Point(6, 124);
+            this.txtNombreActualizado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNombreActualizado.Name = "txtNombreActualizado";
+            this.txtNombreActualizado.Size = new System.Drawing.Size(185, 22);
+            this.txtNombreActualizado.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnEliminar);
+            this.groupBox2.Controls.Add(this.txtID);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(544, 18);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(323, 246);
+            this.groupBox2.TabIndex = 25;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Eliminar";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(6, 88);
+            this.txtID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(301, 22);
+            this.txtID.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ID Categoaria";
+            // 
+            // Categorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1437, 554);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.dgClientes);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Categorias";
+            this.Text = "categoria";
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgClientes)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.DataGridView dgClientes;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.TextBox txtNombreCategoria;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textCategoriaID;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtIDActualizar;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNombreActualizado;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Categoria.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace EjercicioBaseDeDatosWinForm
+{
+    public partial class Categorias : Form
+    {
+        private const string connectionString = @"Data Source=DESKTOP-H0RUUE2;Initial Catalog=Practica3;Integrated Security=True;";
+
+        public Categorias()
+        {
+            InitializeComponent();
+        }
+
+        // ----------------------------------------------------------------------
+        // CARGAR DATOS
+        // ----------------------------------------------------------------------
+        private void cargarDatos()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT CategoriaID, NombreCategoria FROM Categorias;";
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+                            dgClientes.DataSource = dt;
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error al cargar categorías: " + ex.Message, "Error SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Categorias_Load(object sender, EventArgs e)
+        {
+            this.cargarDatos();
+        }
+
+        // ----------------------------------------------------------------------
+        // INSERTAR CATEGORIA
+        // ----------------------------------------------------------------------
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreCategoria.Text))
+            {
+                MessageBox.Show("El nombre de la categoría es obligatorio.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "INSERT INTO Categorias (NombreCategoria) VALUES (@Nombre)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", txtNombreCategoria.Text);
+
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        if (rows > 0)
+                            MessageBox.Show("Categoría insertada correctamente ✔️");
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Error al insertar categoría: " + ex.Message, "Error SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            this.cargarDatos();
+            txtNombreCategoria.Clear();
+        }
+
+        // ----------------------------------------------------------------------
+        // ELIMINAR CATEGORIA
+        // ----------------------------------------------------------------------
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text) || !int.TryParse(txtID.Text, out int categoriaID))
+            {
+                MessageBox.Show("Debe introducir un ID válido de categoría.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "DELETE FROM Categorias WHERE CategoriaID = @ID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@ID", categoriaID);
+
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Categoría eliminada correctamente 🗑️");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se encontró una categoría con ese ID.", "Información");
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Error al eliminar categoría: " + ex.Message, "Error SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            this.cargarDatos();
+            txtID.Clear();
+        }
+
+        // ----------------------------------------------------------------------
+        // ACTUALIZAR CATEGORIA
+        // ----------------------------------------------------------------------
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtIDActualizar.Text) || !int.TryParse(txtIDActualizar.Text, out int categoriaID))
+            {
+                MessageBox.Show("Debe introducir un ID válido para actualizar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtNombreActualizado.Text))
+            {
+                MessageBox.Show("Debe introducir un nuevo nombre para la categoría.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "UPDATE Categorias SET NombreCategoria = @Nombre WHERE CategoriaID = @ID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", txtNombreActualizado.Text);
+                    cmd.Parameters.AddWithValue("@ID", categoriaID);
+
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        if (rows > 0)
+                            MessageBox.Show("Categoría actualizada correctamente 🔄");
+                        else
+                            MessageBox.Show("No se encontró una categoría con ese ID.", "Información");
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Error al actualizar categoría: " + ex.Message, "Error SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            this.cargarDatos();
+            txtIDActualizar.Clear();
+            txtNombreActualizado.Clear();
+        }
+
+        // ----------------------------------------------------------------------
+        // BOTÓN CARGAR DATOS
+        // ----------------------------------------------------------------------
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            this.cargarDatos();
+        }
+
+        private void btnAgregar_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreCategoria.Text))
+            {
+                MessageBox.Show("El nombre de la categoría es obligatorio.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "INSERT INTO Categorias (NombreCategoria) VALUES (@Nombre)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", txtNombreCategoria.Text);
+
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        if (rows > 0)
+                            MessageBox.Show("Categoría insertada correctamente ✔️");
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Error al insertar categoría: " + ex.Message, "Error SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            this.cargarDatos();
+            txtNombreCategoria.Clear();
+        }
+
+        private void btnEliminar_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text) || !int.TryParse(txtID.Text, out int categoriaID))
+            {
+                MessageBox.Show("Debe introducir un ID válido de categoría.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "DELETE FROM Categorias WHERE CategoriaID = @ID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@ID", categoriaID);
+
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Categoría eliminada correctamente 🗑️");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se encontró una categoría con ese ID.", "Información");
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Error al eliminar categoría: " + ex.Message, "Error SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            this.cargarDatos();
+            txtID.Clear();
+
+        }
+
+        private void btnActualizar_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtIDActualizar.Text) || !int.TryParse(txtIDActualizar.Text, out int categoriaID))
+            {
+                MessageBox.Show("Debe introducir un ID válido para actualizar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtNombreActualizado.Text))
+            {
+                MessageBox.Show("Debe introducir un nuevo nombre para la categoría.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "UPDATE Categorias SET NombreCategoria = @Nombre WHERE CategoriaID = @ID";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", txtNombreActualizado.Text);
+                    cmd.Parameters.AddWithValue("@ID", categoriaID);
+
+                    try
+                    {
+                        connection.Open();
+                        int rows = cmd.ExecuteNonQuery();
+                        if (rows > 0)
+                            MessageBox.Show("Categoría actualizada correctamente 🔄");
+                        else
+                            MessageBox.Show("No se encontró una categoría con ese ID.", "Información");
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show("Error al actualizar categoría: " + ex.Message, "Error SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+            this.cargarDatos();
+            txtIDActualizar.Clear();
+            txtNombreActualizado.Clear();
+        }
+    }
+}
+
+
